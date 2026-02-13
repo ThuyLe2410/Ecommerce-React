7865fc1fa48e9b4a0578c4b858b368e5bed9ab13
ThuyLe2410 <ngocthuyle19@gmail.com>
2026-02-13 15:21:39 +0700

    fetch data from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,39 +3,44 @@ import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products').then((response) => {
+    response.json().then((data)=> {
+      console.log(data)
+    })
+  })
   return (
     <>
       <title>Ecommerce Project</title>
       <Header />
 
-      <div class="home-page">
-        <div class="products-grid">
+      <div className="home-page">
+        <div className="products-grid">
           {products.map((product) => {
             return (
               <>
-                <div key={product.id} class="product-container">
-                  <div class="product-image-container">
+                <div key={product.id} className="product-container">
+                  <div className="product-image-container">
                     <img
-                      class="product-image"
+                      className="product-image"
                       src={product.image}
                     />
                   </div>
 
-                  <div class="product-name limit-text-to-2-lines">
+                  <div className="product-name limit-text-to-2-lines">
                     {product.name}
                   </div>
 
-                  <div class="product-rating-container">
+                  <div className="product-rating-container">
                     <img
-                      class="product-rating-stars"
+                      className="product-rating-stars"
                       src={`images/ratings/rating-${product.rating.stars * 10}.png`}
                     />
-                    <div class="product-rating-count link-primary">{product.rating.count}</div>
+                    <div className="product-rating-count link-primary">{product.rating.count}</div>
                   </div>
 
-                  <div class="product-price">${(product.priceCents/100).toFixed(2)}</div>
+                  <div className="product-price">${(product.priceCents/100).toFixed(2)}</div>
 
-                  <div class="product-quantity-container">
+                  <div className="product-quantity-container">
                     <select>
                       <option value="1">1</option>
                       <option value="2">2</option>
@@ -50,14 +55,14 @@ export function HomePage() {
                     </select>
                   </div>
 
-                  <div class="product-spacer"></div>
+                  <div className="product-spacer"></div>
 
-                  <div class="added-to-cart">
+                  <div className="added-to-cart">
                     <img src="images/icons/checkmark.png" />
                     Added
                   </div>
 
-                  <button class="add-to-cart-button button-primary">
+                  <button className="add-to-cart-button button-primary">
                     Add to Cart
                   </button>
                 </div>
